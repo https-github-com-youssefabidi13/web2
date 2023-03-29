f8f736ea33fbd2d08621277e7c25294c9aaba1a3
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-29 16:10:43 +0000

    conception

---
+++ Conception/bd/location_objet.sql
@@ -0,0 +1,345 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: location_object
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `id` int NOT NULL,
+  `ville` varchar(50) NOT NULL,
+  `pays` varchar(50) NOT NULL,
+  `street` varchar(250) NOT NULL,
+  `zip_code` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `annonces`
+--
+
+DROP TABLE IF EXISTS `annonces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `annonces` (
+  `id` int NOT NULL,
+  `partenaire_id` int NOT NULL,
+  `categorie_id` int NOT NULL,
+  `titre` varchar(100) NOT NULL,
+  `description` text,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `prix` decimal(8,2) NOT NULL,
+  `ville` varchar(50) NOT NULL,
+  `jours_minimum` int NOT NULL,
+  `date_creation` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_partenaire_id` (`partenaire_id`),
+  KEY `fk_categorie_id` (`categorie_id`),
+  CONSTRAINT `fk_categorie_id` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `fk_partenaire_id` FOREIGN KEY (`partenaire_id`) REFERENCES `partenaires` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `annonces`
+--
+
+LOCK TABLES `annonces` WRITE;
+/*!40000 ALTER TABLE `annonces` DISABLE KEYS */;
+/*!40000 ALTER TABLE `annonces` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `id` int NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contrat`
+--
+
+DROP TABLE IF EXISTS `contrat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contrat` (
+  `id` int NOT NULL,
+  `client_id` int NOT NULL,
+  `partenaire_id` int NOT NULL,
+  `order_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_partenaires_id` (`partenaire_id`),
+  KEY `fk_order_id` (`client_id`),
+  CONSTRAINT `fk_clients_id2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  CONSTRAINT `fk_order_id` FOREIGN KEY (`client_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `fk_partenaires_id` FOREIGN KEY (`partenaire_id`) REFERENCES `partenaires` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contrat`
+--
+
+LOCK TABLES `contrat` WRITE;
+/*!40000 ALTER TABLE `contrat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contrat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `evaluations`
+--
+
+DROP TABLE IF EXISTS `evaluations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `evaluations` (
+  `id` int NOT NULL,
+  `reservation_id` int NOT NULL,
+  `note` int NOT NULL,
+  `commentaire` text,
+  `type` varchar(20) NOT NULL,
+  `date_creation` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_reservation_id` (`reservation_id`),
+  CONSTRAINT `fk_reservation_id` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evaluations`
+--
+
+LOCK TABLES `evaluations` WRITE;
+/*!40000 ALTER TABLE `evaluations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evaluations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `amount` float NOT NULL,
+  `payement_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `payement_methode` varchar(50) NOT NULL,
+  `payement_status` enum('payee','non_payee') DEFAULT 'non_payee',
+  `order_tracking_number` varchar(255) NOT NULL,
+  `client_id` int NOT NULL,
+  `shipping_address` int NOT NULL,
+  `billing_address` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_clients_id1` (`client_id`),
+  KEY `fk_shipping_id` (`shipping_address`),
+  KEY `fk_billing_id` (`billing_address`),
+  CONSTRAINT `fk_billing_id` FOREIGN KEY (`billing_address`) REFERENCES `address` (`id`),
+  CONSTRAINT `fk_clients_id1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  CONSTRAINT `fk_shipping_id` FOREIGN KEY (`shipping_address`) REFERENCES `address` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partenaires`
+--
+
+DROP TABLE IF EXISTS `partenaires`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `partenaires` (
+  `id` int NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partenaires`
+--
+
+LOCK TABLES `partenaires` WRITE;
+/*!40000 ALTER TABLE `partenaires` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partenaires` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reclamations`
+--
+
+DROP TABLE IF EXISTS `reclamations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reclamations` (
+  `id` int NOT NULL,
+  `client_id` int NOT NULL,
+  `commentaire` text,
+  `response` text,
+  `status` tinyint(1) DEFAULT NULL,
+  `sujet` varchar(20) NOT NULL,
+  `date_creation` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_clients_id` (`client_id`),
+  CONSTRAINT `fk_clients_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reclamations`
+--
+
+LOCK TABLES `reclamations` WRITE;
+/*!40000 ALTER TABLE `reclamations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reclamations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` int NOT NULL,
+  `annonce_id` int NOT NULL,
+  `client_id` int NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `etat` varchar(20) NOT NULL,
+  `date_creation` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_annonce_id` (`annonce_id`),
+  KEY `fk_client_id` (`client_id`),
+  CONSTRAINT `fk_annonce_id` FOREIGN KEY (`annonce_id`) REFERENCES `annonces` (`id`),
+  CONSTRAINT `fk_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-29 15:42:56
